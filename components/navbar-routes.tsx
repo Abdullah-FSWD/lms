87994d0f57111254c6b4752c58ccd1c73e900b0d
abdullah-FSWD <ansarimdabdullah794@gmail.com>
2024-12-17 11:47:16 +0530

    main:feature(dynamic layout created for teacher and guest)

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,23 +1,24 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
 
 export const NavbarRoutes = () => {
-  const pathName = usePathname();
-  const router = useRouter();
-  const isTeacher = pathName?.startsWith("/teacher");
-  const isPlayer = pathName?.startsWith("/chapter");
+  const pathname = usePathname();
+  const isTeacher = pathname?.startsWith("/teacher");
+  const isPlayer = pathname?.startsWith("/chapter");
   return (
     <div className="flex gap-x-2 ml-auto">
       {isTeacher || isPlayer ? (
-        <Button size="sm" variant="ghost">
-          <LogOut className="h-4 w-4 mr-2" />
-          Exit
-        </Button>
+        <Link href="/">
+          <Button size="sm" variant="ghost">
+            <LogOut className="h-4 w-4 mr-2" />
+            Exit
+          </Button>
+        </Link>
       ) : (
         <Link href="teacher/courses">
           <Button size="sm" variant="ghost">
