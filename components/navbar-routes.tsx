6528f68bde466a942c456a5e0aaa3eedc56c35be
abdullah-FSWD <ansarimdabdullah794@gmail.com>
2024-12-17 11:00:34 +0530

    main:feature(teacher mode added)

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,11 +1,31 @@
-"user client";
+"use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { usePathname, useRouter } from "next/navigation";
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 export const NavbarRoutes = () => {
+  const pathName = usePathname();
+  const router = useRouter();
+  const isTeacher = pathName?.startsWith("/teacher");
+  const isPlayer = pathName?.startsWith("/chapter");
   return (
     <div className="flex gap-x-2 ml-auto">
-      <UserButton />
+      {isTeacher || isPlayer ? (
+        <Button size="sm" variant="ghost">
+          <LogOut className="h-4 w-4 mr-2" />
+          Exit
+        </Button>
+      ) : (
+        <Link href="teacher/courses">
+          <Button size="sm" variant="ghost">
+            Teacher mode
+          </Button>
+        </Link>
+      )}
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
