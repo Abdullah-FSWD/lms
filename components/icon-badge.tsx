14bc35bc663816b2ac45ccbc0a74768b1e11d80a
abdullah-FSWD <ansarimdabdullah794@gmail.com>
2024-12-31 11:31:10 +0530

    fix icon badge

--- components/icon-badge.tsx
+++ components/icon-badge.tsx
@@ -7,32 +7,51 @@ const backgroundVariants = cva(
   {
     variants: {
       variant: {
-        defualt: "bg-sky-100",
+        default: "bg-sky-100",
         success: "bg-emerald-100",
       },
-      iconVariant: {
-          default: "text-sky-700",
-          success: "text emerald-700",
-      },
+      // iconVariant: {
+      //   default: "text-sky-700",
+      //   success: "text emerald-700",
+      // },
       size: {
-        defualt: "p-2",
+        default: "p-2",
         sm: "p-1",
       },
-      defaultVariants:{
-        variant:'default',
-        size:"default"
-      }
+      defaultVariants: {
+        variant: "default",
+        size: "default",
+      },
     },
   }
 );
 
-const iconVariants = cva(
-    "",
-    {
-        variants:{
-            variant:{
-                default:"text-sky-"
-            }
-        }
-    }
-)
+const iconVariants = cva("", {
+  variants: {
+    variant: {
+      default: "text-sky-700",
+      success: "text-emerald-700",
+    },
+    size: {
+      default: "h-8 w-8",
+      sm: "h-4 w-4",
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+});
+
+type BackgroundVariantsProps = VariantProps<typeof backgroundVariants>;
+type IconVariantsProps = VariantProps<typeof iconVariants>;
+
+interface IconBadgeProps extends BackgroundVariantsProps, IconVariantsProps {
+  icon: LucideIcon;
+}
+
+export const IconBadge = ({ icon: Icon, variant, size }: IconBadgeProps) => {
+  <div className={cn(backgroundVariants({ variant, size }))}>
+    <Icon className={cn(iconVariants({ variant, size }))} />
+  </div>;
+};
