6f8cef69652e82d9cb0f29f6598f375e3d17b55d
abdullah-FSWD <ansarimdabdullah794@gmail.com>
2025-01-04 17:23:52 +0530

    uploadthing completed

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -6,13 +6,13 @@ import { ourFileRouter } from "@/app/api/uploadthing/core";
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
-  endPoint: keyof typeof ourFileRouter;
+  endpoint: keyof typeof ourFileRouter;
 }
 
-export const FileUpload = ({ onChange, endPoint }: FileUploadProps) => {
+export const FileUpload = ({ onChange, endpoint }: FileUploadProps) => {
   return (
     <UploadDropzone
-      endpoint={endPoint}
+      endpoint={endpoint}
       onClientUploadComplete={(res) => {
         onChange(res?.[0].url);
       }}
