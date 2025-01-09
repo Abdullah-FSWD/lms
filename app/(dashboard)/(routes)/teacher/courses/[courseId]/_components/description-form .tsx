9d66b907621233a35543b87381e022c06e250646
abdullah-FSWD <ansarimdabdullah794@gmail.com>
2025-01-09 11:45:29 +0530

    Revert "feature/lms-course:refactor(modified function name)"
    This reverts commit 1511255943f2116ca772223b274fe678cc5af220.

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form .tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/description-form .tsx
@@ -61,7 +61,6 @@ export default function DescriptionForm({
 
   return (
     <div className="mt-6 border bg-slate-100 rounded-md p-4">
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
       <div className="font-medium flex items-center justify-center">
         Course Description
         <Button onClick={toggleEdit} variant="ghost">
@@ -79,7 +78,7 @@ export default function DescriptionForm({
         <p
           className={cn(
             "text-sm mt-2",
-            !initialData.description && "text-slate-500 italic"
+            !initialData.description && "text-slate-500 italic",
           )}
         >
           {initialData.description || "No description"}
@@ -115,5 +114,4 @@ export default function DescriptionForm({
       )}
     </div>
   );
-};
-export default DescriptionForm;
+}
