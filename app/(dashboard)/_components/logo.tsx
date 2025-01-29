0542c0921a53c252a05327e7259d4f3438b37f18
abdullah-fswd <ansarimdabdullah794@gmail.com>
2025-01-29 12:49:25 +0530

    feat: added logo and updated metadata

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const Logo = () => {
   return (
     <Image
-      className="m-4"
+      className="m-4 mt-0"
       height={130}
       width={130}
       alt="logo"
